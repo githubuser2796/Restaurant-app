096d097c5afef133a40cb353bd9d0876c012111a
Neha <nehajagtap27@gmail.com>
2020-06-22 15:53:43 +0530

    new

--- mod5-solution/src/public/public.routes.js
+++ mod5-solution/src/public/public.routes.js
@@ -40,6 +40,23 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state('public.signup',{
+      url: '/signup',
+      templateUrl: 'src/public/sign-up/signup.html',
+      controller: 'SignUpController',
+      controllerAs: 'signupCtrl'
+    })
+    .state('public.myinfo', {
+      url: '/myinfo',
+      templateUrl: 'src/public/my-info/my-info.html',
+      controller: 'MyInfoController',
+      controllerAs: 'myInfoCtrl',
+      resolve: {
+        userInfo: ['MyInfoService', function(MyInfoService) {
+          return MyInfoService.getMyinfo();
+        }]
+      }
     });
 }
 })();
